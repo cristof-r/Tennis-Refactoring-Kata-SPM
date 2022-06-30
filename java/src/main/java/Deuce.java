c23cbf61c145de3f203b521a73c7a24a9e9ff393
Cristof Rojas <cristof.rojas@student.uibk.ac.at>
2022-06-30 10:40:53 +0200

    moved deuce method from game to deuce class

--- java/src/main/java/Deuce.java
+++ java/src/main/java/Deuce.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Deuce implements ResultProvider {
     private final TennisGame4 game;
     private final ResultProvider nextResult;
@@ -7,9 +9,15 @@ public class Deuce implements ResultProvider {
         this.nextResult = nextResult;
     }
 
+    boolean isDeuce() {
+        return game.getServer().getScore() >= 3
+                && game.getReceiver().getScore() >= 3
+                && (Objects.equals(game.getServer().getScore(), game.getReceiver().getScore()));
+    }
+    
     @Override
     public TennisResult getResult() {
-        if (game.isDeuce()) {
+        if (isDeuce()) {
             return new TennisResult("Deuce", "");
         }
         return nextResult.getResult();

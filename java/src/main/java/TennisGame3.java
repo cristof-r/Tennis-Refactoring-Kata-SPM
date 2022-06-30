9beb401fd3a57ed03cdd8ae2bfe975b45bbff608
FloGerhold <florian.gerhold@gmail.com>
2022-06-30 10:09:59 +0200

    included Player class instead of name and points

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -1,48 +1,46 @@
 
 public class TennisGame3 implements TennisGame {
 
-    private int player1;
-    private int player2;
-    private final String player1Name;
-    private final String player2Name;
+    private final Player player1;
+    private final Player player2;
 
     public TennisGame3(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
     }
 
     public String getScore() {
         String standing;
         if (scoreIsLow()) {
             String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            standing = points[player1];
+            standing = points[player1.getScore()];
             return getLowScoreString(standing, points);
         } else {
-            if (player1 == player2)
+            if (player1.getScore().equals(player2.getScore()))
                 return "Deuce";
-            standing = player1 > player2 ? player1Name : player2Name;
+            standing = player1.getScore() > player2.getScore() ? player1.getName() : player2.getName();
             return getHighScoreString(standing);
         }
     }
 
     private String getHighScoreString(String standing) {
-        return ((player1 - player2) * (player1 - player2) == 1) ? "Advantage " + standing : "Win for " + standing;
+        return ((player1.getScore() - player2.getScore()) * (player1.getScore() - player2.getScore()) == 1) ? "Advantage " + standing : "Win for " + standing;
     }
 
     private String getLowScoreString(String standing, String[] points) {
-        return (player1 == player2) ? standing + "-All" : standing + "-" + points[player2];
+        return (player1.getScore().equals(player2.getScore())) ? standing + "-All" : standing + "-" + points[player2.getScore()];
     }
 
     public void wonPoint(String playerName) {
         if (playerName.equals("player1"))
-            this.player1 += 1;
+            player1.wonPoint();
         else
-            this.player2 += 1;
+            player2.wonPoint();
         
     }
 
     private boolean scoreIsLow(){
-        return player1 < 4 && player2 < 4 && !(player1 + player2 == 6);
+        return player1.getScore() < 4 && player2.getScore() < 4 && !(player1.getScore() + player2.getScore() == 6);
     }
 
 }
